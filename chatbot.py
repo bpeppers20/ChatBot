54bcddf804f10c5bfa6ee871664769fa6ee54027
bpeppers20 <bryantpeppers@gmail.com>
2020-09-01 00:46:32 -0500

    Commit 2 More Secure Implementation

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,16 @@
 import tweepy
-#Authenicaton
-consumer_key = '4ky21bmQvioRcpu7kBgXe54ew'
-consumer_secret = 'C7aAUqeO2I1qb25aNK9CgZ245bvJcTrP5DaWUZFpz2gC6VxSPP'
+import os
 
-key = '1286112733340020738-Vxj95fv3ji9wQ5iKwvdsNED98xEm8k'
-secret = 'NEfkgdzvxBtrGTDScvSOv0xCUu6sr7Gvmx6H1YiO56Ofi'
+# Auth Variables
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+KEY = os.environ.get('KEY')
+SECRET = os.environ.get('SECRET')
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(key,secret)
 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(KEY,SECRET)
 api = tweepy.API(auth)
 #Authenicaton
-api.update_status("Boop")
+api.update_status("Hello Twitter Septeber 2020 2!")
+print("Request Submitted")
