8d62e458e93e0e5a4855e924384c424ff6729353
bpeppers20 <bryantpeppers@gmail.com>
2020-09-07 22:41:55 -0500

    Commit 2: Schedular Functions and Library works

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,26 @@
 import tweepy
+import schedule
 import os
+import time
 
-# Auth Variables
-CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
-CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
-KEY = os.environ.get('KEY')
-SECRET = os.environ.get('SECRET')
+def job():
+    # Auth Variables
+    CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+    CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+    KEY = os.environ.get('KEY')
+    SECRET = os.environ.get('SECRET')
 
+    #Authenicaton
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(KEY,SECRET)
+    api = tweepy.API(auth)
+    #Make Post about event
+    api.update_status("Hi! @Sebbiesuperior and @bpeppers1. Can y'all make it to D&D today? Its today at 7:00pm!")
+    print("Request Sent")
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(KEY,SECRET)
-api = tweepy.API(auth)
-#Authenicaton
-api.update_status("Hello Twitter Septeber 2020 Test 3!")
-print("Request Submitted")
+#Time
+schedule.every().friday.at("18:30").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
