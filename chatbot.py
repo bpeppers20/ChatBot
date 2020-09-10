4ea79e8f00a4ad84323dda3628d8afb3f7f189b2
bpeppers20 <bryantpeppers@gmail.com>
2020-09-09 19:53:40 -0500

    Commit 2: Day Selector works

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,8 @@ import schedule
 import os
 import time
 
+
+#Send Status Post to twitter dev account
 def job():
     # Auth Variables
     CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
@@ -15,13 +17,69 @@ def job():
     auth.set_access_token(KEY,SECRET)
     api = tweepy.API(auth)
     #Make Post about event
-    api.update_status("This is a demo of the schedular :)")
+    api.update_status("This is a demo of the scheduler :)")
     print("Request Sent")
 
-#Time
-schedule.every().friday.at("18:30").do(job)
-schedule.every().wednesday.at("18:30").do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+#Functions to run on select day and time for that day
+def selectSunday():
+    schedule.every().sunday.at(timer).do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def selectMonday():
+    schedule.every().monday.at(timer).do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def selectTuesday():
+    schedule.every().tuesday.at(timer).do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def selectWednesday():
+    schedule.every().wednesday.at(timer).do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def selectThursday():
+    schedule.every().thursday.at(timer).do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def selectFriday():
+    schedule.every().friday.at(timer).do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def selectSaturday():
+    schedule.every().saturday.at(timer).do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def startSchedule(case):
+    switch = {
+        0: selectSunday,
+        1: selectMonday,
+        2: selectTuesday,
+        3: selectWednesday,
+        4: selectThursday,
+        5: selectFriday,
+        6: selectSaturday
+    }
+    func = switch.get(case, lambda : 'Day not valid or format')
+    return func() #return the function of the selected day
+
+day = int(input("Enter the day you wish the post to be released. 0 = sun, 1 = mon, 2 = tues, etc...\n"))
+
+timer = str(input("Enter the time you want the post to be released. Needs to be in military time\n")) #Should be string for day function a("")
+
+startSchedule(day)
